c8f389d3bbfd3b39fd15bacbe476328aff34ca11
Chris <chrisvass1@gmail.com>
2017-12-08 18:46:02 -0500

    fixed game

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
+var game_over = new Image();
+game_over.src = "game_over.png";
+
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-var a = .5;
-var v = 8;
-var t = 0;
+var keys = [];
+var tick = 0;
 document.addEventListener('keydown',function (e) {
-  if(e.keyCode == 32){
-    t=0
-  }
-})
+  keys[e.keyCode] = true;
+}, false)
+document.addEventListener('keyup',function (e) {
+  keys[e.keyCode] = false;
+  if(e.keyCode == 32)flappy.jump = true;
+}, false)
 
+//returns random inclusive int
+function rand(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//player object
 var flappy = {
   x : c.width/2,
   y : c.height/2,
+  a : .5, //acceleration
+  v : 8, //velocity
+  t : 0, //time
   size : 25,
-  draw : function () {
+  alive : true,
+  jump : true,
+  update : function () {
+    if(keys[32] && this.jump && flappy.alive){
+      this.t = 0;
+      this.jump = false;
+    }
+    this.y+= this.a*this.t - this.v;
+    if (this.y+this.size>c.height) {
+      this.y = c.height-this.size
+    }
+    this.t++;
+    ctx.fillStyle = 'yellow';
     ctx.fillRect(this.x,this.y,this.size,this.size);
   }
 }
 
-var test = new pipe(160,130);
-
-setInterval(function () {
-  ctx.clearRect(0,0,c.width,c.height);
-  test.update();
-  flappy.y+= a*t - v;
-  flappy.draw();
-  t++;
-},18);
-
-
+//pipe object
 function pipe(tlength, blength){
   this.tx = c.width;
   this.ty = 0;
@@ -43,10 +60,57 @@ function pipe(tlength, blength){
   this.update = function () {
     this.tx--;
     this.bx--;
-    if (flappy.y<this.ty+this.th && flappy.x<this.tx+this.tw) {
-      console.log('ECH');
+    if ((flappy.y+flappy.size>this.by || flappy.y<this.ty+this.th) && ((flappy.x+flappy.size<this.tx+this.tw && flappy.x+flappy.size>this.tx) || (flappy.x<this.tx+this.tw && flappy.x>this.tx))) {
+      flappy.alive = false;
     }
+  }
+  this.draw = function () {
+    ctx.fillStyle = 'black';
     ctx.fillRect(this.tx,this.ty,this.tw,this.th);
     ctx.fillRect(this.bx,this.by,this.bw,this.bh);
   }
 }
+var pipes = [];
+
+function newGame() {
+  pipes = [];
+  tick = 0;
+  flappy.x = c.width/2;
+  flappy.y = c.height/2;
+  flappy.t = 0;
+  flappy.alive  = true;
+}
+
+//game loop
+function loop() {
+  ctx.clearRect(0,0,c.width,c.height); //clear screen
+  //draw pipes
+  for (var i = 0; i < pipes.length; i++) {
+    pipes[i].draw();
+  }
+  flappy.update(); //update player
+
+  //events only run while game is in progress
+  if (flappy.alive) {
+    for (var i = 0; i < pipes.length; i++) {
+      pipes[i].update();
+    }
+    if (tick%200 == 0) {
+      let tlength = rand(c.width/5,c.width/5*4);
+      pipes.push(new pipe(tlength, c.height-(tlength+ 110)));
+    }
+  }else {
+    ctx.drawImage(game_over,0,0);
+
+    if (keys[32] &&  flappy.jump) {
+      newGame();
+    }
+  }
+
+  tick++;
+  window.requestAnimationFrame(loop);
+}
+
+
+//begin loop
+window.requestAnimationFrame(loop);
